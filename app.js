0db5436a427b56994b125fb420cff3f18bb0b959
orla90 <orla90@inbox.ru>
2022-05-21 13:38:42 +0300

    fix: change class names using BEM according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -47,12 +47,10 @@ var createNewTaskElement=function(taskString){
     checkBox.classList.add("todo-list__input", "todo-list__input_checkbox");
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
-    editButton.classList.add("todo-list__button");
     
-
-    deleteButton.className="delete";
-    deleteButton.classList.add("todo-list__button");
+    editButton.classList.add("todo-list__button", "todo-list__button_edit");
+    
+    deleteButton.classList.add("todo-list__button", "todo-list__button_delete");
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.classList.add("todo-list__delete-img");
     deleteButton.appendChild(deleteButtonImg);
@@ -94,7 +92,7 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
+    var editBtn=listItem.querySelector(".todo-list__button_edit");
     var containsClass=listItem.classList.contains("todo-list__edit-mode");
     //If class of the parent is .todo-list__edit-mode
     if(containsClass){
@@ -166,8 +164,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector("button.todo-list__button_edit");
+    var deleteButton=taskListItem.querySelector("button.todo-list__button_delete");
 
 
     //Bind editTask to edit button.
